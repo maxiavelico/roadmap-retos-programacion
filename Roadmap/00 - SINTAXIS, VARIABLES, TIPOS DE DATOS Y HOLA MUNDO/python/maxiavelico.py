161e1d0b0e0f7550504abdde485c2e30e91a1acc
maxiavelico <maxheras140891@gmail.com>
2025-08-04 09:36:27 -0500

    Create maxiavelico.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/maxiavelico.py
@@ -0,0 +1,13 @@
+# Este es el comentario inicial del ejercicio#
+''' Este es un comentario de varias lineas
+a propósito aqui dejo el link de Python: https://www.python.org/
+y con eso cierro este comentario'''
+Nombre = 'Max'
+Edad = 33.5
+edad real = 33
+hobby = 'estoy aprendiendo a programar'
+definicion_altura = true
+definicion_altura = false
+TIPO_MUSICA = phonk
+TIPO_MUSICA2 = HOUSE
+print (¡Hola, el nombre de mi lenguaje es [Python]
